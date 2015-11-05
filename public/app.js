a24bb555aaa41b288e354c6b8998f6c258ac7dc9
Brandi Soucy <bbsoucy@gmail.com>
2015-11-05 16:09:16 -0600

    Making adjustments to index and css

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
+var timer = document.querySelector('.time');
+var colorBar = document.querySelector('.color');
+
 (function() {
-
-
   window.setInterval(() => {
     var currentTime = new Date();
     console.log(currentTime.toTimeString());
