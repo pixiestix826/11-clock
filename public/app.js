33a191e1563f6a086dd106c7e4908ac7acfa3b6b
Brandi Soucy <bbsoucy@gmail.com>
2015-11-05 17:41:37 -0600

    Adding minor changes to js file

--- public/app.js
+++ public/app.js
@@ -1,11 +1,11 @@
+var timer = document.querySelector('.time');
+var colorBar = document.querySelector('.color');
 
 (function() {
-  var timer = document.querySelector('.time');
-  var colorBar = document.querySelector('.color');
   window.setInterval(() => {
     var currentTime = new Date();
     console.log(currentTime.toTimeString());
-    var curr = `${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}`
+    var curr = `${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}`;
     timer.innerHTML = curr;
   }, 1000);
 })();
