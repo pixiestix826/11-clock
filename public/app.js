d62a969a07f6599ba28db36f840789473dcbda6c
Brandi Soucy <bbsoucy@gmail.com>
2015-11-05 16:52:33 -0600

    displaying timer in dom

--- public/app.js
+++ public/app.js
@@ -1,10 +1,11 @@
-var timer = document.querySelector('.time');
-var colorBar = document.querySelector('.color');
 
 (function() {
+  var timer = document.querySelector('.time');
+  var colorBar = document.querySelector('.color');
   window.setInterval(() => {
     var currentTime = new Date();
     console.log(currentTime.toTimeString());
-    timer.innerHTML = currentTime;
+    var curr = `${currentTime.getHours()}:${currentTime.getMinutes()}:${currentTime.getSeconds()}`
+    timer.innerHTML = curr;
   }, 1000);
 })();
