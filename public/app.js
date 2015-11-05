e8fd6e8216e9c630236354eead97a306d6df7a13
Brandi Soucy <bbsoucy@gmail.com>
2015-11-04 22:27:27 -0600

    Working on javascript

--- public/app.js
+++ public/app.js
@@ -1,10 +1,19 @@
 (function() {
   var currentTime = document.querySelector('.time');
+  var startColor = document.querySelector('.color');
+  var startTime = new Date();
+  var intervalTime = 0;
+  var timeToRender = 100;
+
+  window.setInterval(() => {
+    var elapsedTime = new Date() - startTime;
+    currentTime.innerHTML = elapedTime;
+  }, timeToRender);
+
+  window.setInterval(() => {
+    intervalTime += timeToRender;
+    startColor.innerHTML = intervalTime;
+  }, timeToRender);
 });
 
-var d = new Date();
-d.toDateString();
-d.getHours();
-d.getMinutes();
-d.getSeconds();
-console.log(d);
+// console.log(currentTime.toTimeString());
