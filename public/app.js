e82fb1b68a2653b6c590ba3dd1b90fbed63dab29
Brandi Soucy <bbsoucy@gmail.com>
2015-11-05 15:36:33 -0600

    Adding window interval

--- public/app.js
+++ public/app.js
@@ -1,19 +1,8 @@
 (function() {
-  var currentTime = document.querySelector('.time');
-  var startColor = document.querySelector('.color');
-  var startTime = new Date();
-  var intervalTime = 0;
-  var timeToRender = 100;
+
 
   window.setInterval(() => {
-    var elapsedTime = new Date() - startTime;
-    currentTime.innerHTML = elapedTime;
-  }, timeToRender);
-
-  window.setInterval(() => {
-    intervalTime += timeToRender;
-    startColor.innerHTML = intervalTime;
-  }, timeToRender);
-});
-
-// console.log(currentTime.toTimeString());
+    var currentTime = new Date();
+    console.log(currentTime.toTimeString());
+  }, 1000);
+})();
